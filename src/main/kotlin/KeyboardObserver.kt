86e3fbf17b4fd84b7a01b07e75e01ce1d2f3d7ee
John Doe <johndoe@example.com>
2021-04-09 10:29:39 +0300

    fixed bags

--- src/main/kotlin/KeyboardObserver.kt
+++ src/main/kotlin/KeyboardObserver.kt
@@ -6,30 +6,30 @@ import java.awt.event.KeyListener
 import java.util.*
 import java.util.concurrent.ArrayBlockingQueue
 import javax.swing.JFrame
+import kotlin.system.exitProcess
 
 class KeyboardObserver : Thread() {
     private val keyEvents: Queue<KeyEvent> = ArrayBlockingQueue(100)
-    private var frame: JFrame? = null
+    private var frame: JFrame = JFrame("KeyPress Tester")
     override fun run() {
-        frame = JFrame("KeyPress Tester")
-        frame!!.title = "Transparent JFrame Demo"
-        frame!!.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-        frame!!.isUndecorated = true
-        frame!!.setSize(400, 400)
-        frame!!.extendedState = JFrame.MAXIMIZED_BOTH
-        frame!!.layout = GridBagLayout()
-        frame!!.opacity = 0.0f
-        frame!!.isVisible = true
-        frame!!.addFocusListener(object : FocusListener {
+        frame.title = "Transparent JFrame Demo"
+        frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        frame.isUndecorated = true
+        frame.setSize(400, 400)
+        frame.extendedState = JFrame.MAXIMIZED_BOTH
+        frame.layout = GridBagLayout()
+        frame.opacity = 0.0f
+        frame.isVisible = true
+        frame.addFocusListener(object : FocusListener {
             override fun focusGained(e: FocusEvent) {
                 //do nothing
             }
 
             override fun focusLost(e: FocusEvent) {
-                System.exit(0)
+                exitProcess(0)
             }
         })
-        frame!!.addKeyListener(object : KeyListener {
+        frame.addKeyListener(object : KeyListener {
             override fun keyTyped(e: KeyEvent) {
                 //do nothing
             }
